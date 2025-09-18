ea15f47775b4a16fce7526f39e1c368c9b3bbd8d
M4X809 <github@j-pet.de>
2025-09-18 11:29:28 +0200

    Refactor calculation logic in Aufgabe1.py to improve performance and readability.

---
+++ 18.09.25/Aufgaben1-5.py
@@ -0,0 +1,13 @@
+# Es fehlte ein " und es sollte warscheineich kein Kommentar sein.
+"""print('Das ist mein Programm')"""
+
+# Semantik Fehler, wird als Kommentar nicht ausgeführt.
+print("----------------")
+# "print" falsch geschrieben und ein fehlendes "
+print("Hello World")
+# "print" falsch geschrieben und eine fehlende )
+print("Das hat geklappt")
+
+zahl = 1
+print(f"Zahl1: {zahl}")
+print("Zahl1: ", zahl)
